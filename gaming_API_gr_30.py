bf7e16fe56b2614a46334e3eeaedb5900e0d1eb7
rimnebi01 <flutunebi@gmail.com>
2025-10-13 15:15:54 +0200

    Update gaming_API_gr_30.py

--- gaming_API_gr_30.py
+++ gaming_API_gr_30.py
@@ -1,5 +1,51 @@
 from gaming_tools import *
 
+def new_game():
+    """Reset all the game and set money to 50 for the team
+    """
+    reset_game()
+    set_team_money(50)
+
+
+
+def new_character(character:str, variety:str):
+    """Create a new player to the game and set team money to 50 for the team
+    
+    Parameters
+    ----------
+    character (str): name of the new character 
+    type (str): the type of the player who can be 'dwarf', 'elf', 'healer', 'wizard' or 'necromancer'
+
+    Note
+    ----
+    The character can't exist 
+    
+    """
+
+    set_team_money(50)
+    if character_exists(character) == True:
+        print("This character already exist, please try another one")
+    
+    
+    if variety  == 'dwarf':
+        strength = random.randint(10, 15)
+        life = random.randint(10, 15)
+    elif variety == 'elf':
+        strength = random.randint(15, 25)
+        life = random.randint(15, 25)
+    elif variety == 'healer' or variety == 'wizard' or variety == 'necromancer' :
+        strength = random.randint(5, 15)
+        life = random.randint(5, 15)
+
+    if variety == 'dwarf' or variety=='healer' or variety=='necromancer':
+        reach = 'short'
+    elif variety == 'elf' or variety =='wizard':
+        reach = 'long'
+
+    add_new_character(character,variety,reach,strength,life)
+    print('The new character have been aded and your name is: ', character, '\n Your variety is: ', variety, '\n Your reach is', reach, '\n Your strengh is:', strength, '\n Your life is:', life )
+
+
 def attack(character:str, creature:str):
     """manage the combat part
 
@@ -68,4 +114,5 @@ def attack(character:str, creature:str):
         else:
             set_character_life(character, -1)
             print("%s is dead" %character)
+
 
