9aa5c51b91d95719dca455f59ceecd58f5453941
sheepname <73659691+sheepname@users.noreply.github.com>
2025-10-13 15:17:23 +0200

    Update gaming_API_gr_30.py
    add add_creature()

--- gaming_API_gr_30.py
+++ gaming_API_gr_30.py
@@ -115,4 +115,21 @@ def attack(character:str, creature:str):
             set_character_life(character, -1)
             print("%s is dead" %character)
 
+def createcreature():
+    """Adds a creature
+    Creature name is randomized
+    Creature reach is chosen between short or long
+    Creature strength is set between 1 and 10 * 1+number of defeated creatures
+    Creature life is set between 1 and 10 * 1+number of defeated creatures"""
+    if not is_there_a_creature():
+        cname=get_random_creature_name()
+        creach=random.choice(["short","long"])
+        cstrength=random.randint(1,10)*(1+get_nb_defeated())
+        clife=random.randint(1,10)*(1+get_nb_defeated())
+        add_creature(cname,creach,cstrength,clife)
+        print("A creature has been added (Name:%s Reach:%s Strength:%d Life:%d)"%(cname,creach,cstrength,clife))
+        return(cname,creach,cstrength,clife)
+    else:
+        print("A creature already exists")
+
 
