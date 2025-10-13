5aaa7989156d4cb32f92655c2e0a942ee3e19f96
rimnebi01 <flutunebi@gmail.com>
2025-10-13 14:36:31 +0200

    Update fluturim.py

--- fluturim.py
+++ fluturim.py
@@ -1,6 +1,7 @@
-import gaming_tools
+from gaming_tools import * 
 import random
 
+reset_game()
 
 def new_character(character, variety):
     """Create a new player to the game and set team money to 50 for the team
@@ -11,31 +12,27 @@ def new_character(character, variety):
     type (str): the type of the player who can be 'dwarf', 'elf', 'healer', 'wizard' nor 'necromancer'
     
     """
-    gaming_tools.set_team_money(50)
-    if gaming_tools.character_exists(character) == True:
+    set_team_money(50)
+    if character_exists(character) == True:
         print("This character already exist, please try another one")
     
     
-    variety = gaming_tools.get_character_variety(character)
-    life = gaming_tools.get_character_life(character)
-    reach = gaming_tools.get_creature_reach(character)
-
-    if variety and life  == 'dwarf':
+    if variety  == 'dwarf':
         strength = random.randint(10, 15)
         life = random.randint(10, 15)
-    elif variety and life == 'elf':
+    elif variety == 'elf':
         strength = random.randint(15, 25)
         life = random.randint(15, 25)
-    elif variety and life == 'healer' or 'wizard' or'necromancer' :
+    elif variety == 'healer' or variety == 'wizard' or variety == 'necromancer' :
         strength = random.randint(5, 15)
         life = random.randint(5, 15)
 
+    if variety == 'dwarf' or variety=='healer' or variety=='necromancer':
+        reach = 'short'
+    elif variety == 'elf' or variety =='wizard':
+        reach = 'long'
 
-
-    gaming_tools.set_character_strength(character, strength)
-    gaming_tools.set_character_life(character,life)
-
-    gaming_tools.add_new_character(character,variety,reach,strength,life)
-
+    add_new_character(character,variety,reach,strength,life)
+    print('The new character have been aded')
 
 new_character('Test1','elf')
