7d18042259d3a82e0f7c173021d1e04348d10bf8
rimnebi01 <flutunebi@gmail.com>
2025-10-14 11:02:25 +0200

    finale_V1 gaming_API_gr_30.py

--- gaming_API_gr_30.py
+++ gaming_API_gr_30.py
@@ -1,13 +1,10 @@
 from gaming_tools import *
-import random
 
 def new_game():
     """Reset all the game and set money to 50 for the team
     """
     reset_game()
     set_team_money(50)
-
-
 
 def new_character(character:str, variety:str):
     """Create a new player to the game and set team money to 50 for the team
@@ -43,8 +40,43 @@ def new_character(character:str, variety:str):
         reach = 'long'
 
     add_new_character(character,variety,reach,strength,life)
-    print('The new character have been added and your name is: ', character, '\n Your variety is: ', variety, '\n Your reach is', reach, '\n Your strength is:', strength, '\n Your life is:', life )
+    print('The new character have been added and your name is: ', character, '\n Your variety is: ', variety, '\n Your reach is:', reach, '\n Your strength is:', strength, '\n Your life is:', life )
 
+def create_new_creature():
+
+    """Adds a creature. 
+    Creature name is randomized.
+    Creature reach is chosen between short or long.
+    Creature strength is set between 1 and 10 * 1+number of defeated creatures.
+    Creature life is set between 1 and 10 * 1+number of defeated creatures.
+    """
+  
+    if not is_there_a_creature():
+        creature_name=get_random_creature_name()
+        creature_reach=random.choice(["short","long"])
+        creature_strength=random.randint(1,10)*(1+get_nb_defeated())
+        creature_life=random.randint(1,10)*(1+get_nb_defeated())
+        add_creature(creature_name,creature_reach,creature_strength,creature_life)
+        print("A creature has been added (Name:%s Reach:%s Strength:%d Life:%d)"%(creature_name,creature_reach,creature_strength,creature_life))
+        return(creature_name,creature_reach,creature_strength,creature_life)
+    else:
+        print("A creature already exists")
+
+def character_alive(character):
+    """Checks if the character is alive
+    Parameter
+    __________
+    character: the name of the character (str)
+    
+    Return
+    __________
+    result: True if the character is alive ,False otherwise (bool)
+    """
+    if get_character_life(character)>0:
+        return True
+    
+    else:
+        return False
 
 def attack(character:str, creature:str):
     """Manage the combat part
@@ -114,26 +146,130 @@ def attack(character:str, creature:str):
         else:
             set_character_life(character, -1)
             print("%s is dead" %character)
-
-def create_new_creature():
-    """Adds a creature. 
-    Creature name is randomized.
-    Creature reach is chosen between short or long.
-    Creature strength is set between 1 and 10 * 1+number of defeated creatures.
-    Creature life is set between 1 and 10 * 1+number of defeated creatures.
+            
+def use_special_power(character,target):
+    """Let the character use the special power.
+    
+    Parameter
+    __________
+    character : the name of the given character (str)
+    target: the name of the target (str)
+    
     """
-  
-    if not is_there_a_creature():
-        creature_name=get_random_creature_name()
-        creature_reach=random.choice(["short","long"])
-        creature_strength=random.randint(1,10)*(1+get_nb_defeated())
-        creature_life=random.randint(1,10)*(1+get_nb_defeated())
-        add_creature(creature_name,creature_reach,creature_strength,creature_life)
-        print("A creature has been added (Name:%s Reach:%s Strength:%d Life:%d)"%(creature_name,creature_reach,creature_strength,creature_life))
-        return(creature_name,creature_reach,creature_strength,creature_life)
+    #First ckeck if the character exists
+    
+    if character_exists(character):
+        #check if the character is alive
+        
+        type = get_character_variety(character)
+        
+        if character_alive(character) : 
+            
+            #check the variety of the character  
+            
+            if type=='necromancer':
+                necromancer_power(target)
+            
+            elif type =='wizard':
+                wizard_power(target)
+            
+            elif type =='healer':
+                healer_power(target)
+            else:
+                print('The character does not have a special power')
+        else:
+            print('The character is dead. Can not use special power.')   
+    
     else:
-        print("A creature already exists")
+        print('The character does not exist.')
 
+def necromancer_power(target):
+    """Let the necromancer revive the target with 10 points of live for 75 pieces of gold.
+
+    Parameter
+    ________
+    target : the name of the character we want to revive (str)
+    
+    """
+    
+    # Checks if the target exist
+    if character_exists(target):
+        
+        # Checks if the team has enough money (75 gold)
+        if get_team_money()>=75:
+            
+            # Checks if the target is dead
+            if character_alive(target)==False :
+            
+                set_character_life(target,10)
+            
+                # it cost 75 pieces of gold to use the power
+                set_team_money(get_team_money()-75)
+            else:
+            
+                print('The character is not dead. No need to revive.')
+        
+        else:
+            print('Not enough money.')
+
+    else:
+        print('The character does not exist.')
+
+def wizard_power(target):
+    """Let the wizard reduce the live of the creature  by half for 20 pieces of gold
+     
+     Parameter
+     ___________
+     target: the name of the creature (str)
+     """
+     
+     #check if the creature exists
+    if creature_exists(target):
+        
+        # check if the team has enough money.(20 gold)
+        if get_team_money()>=20:
+
+            set_creature_life(target,get_creature_life(target)//2)
+            #it cost 20 pieces of gold
+            set_team_money(get_team_money()-20)
+        else:
+
+            print('Not enough money.')
+    else:
+
+        print('The creature does not exist.')  
+
+def healer_power(target):
+    """Lets the healer heal a character in the team for 10 pieces of gold
+    
+    Parameter
+    __________
+    target : the name of the character we heal
+    """  
+    # Checks if the target exists 
+    if character_exists(target):
+
+        #check if the team has enough money (5 gold)
+        if get_team_money()>=5:
+
+            #Check if the target is alive
+            if character_alive(target):
+
+                set_character_life(target,get_character_life(target)+10)
+                #it costs 5 pieces of gold
+
+                set_team_money(get_team_money()-5)
+            
+            else:
+
+                print('The character is dead. Can not heal.')
+       
+        # Checks if the target is dead
+        else:
+
+            print('Not enough money.')
+    else: 
+        print('The character does not exist.')
 
 def lvl_up(character):
     """Handles the evolution of a given character. (Strength and life bonuses)
@@ -157,7 +293,7 @@ def lvl_up(character):
 
     money = get_team_money()
 
-# Vérifie si l'équipe est soldable
+    # Vérifie si l'équipe est soldable
     if money < 4:
         print("You have not enough money")
     else:
@@ -184,6 +320,7 @@ def lvl_up(character):
                 return
         else:
             print("You didn't win a life bonus")
+
 
 
 
